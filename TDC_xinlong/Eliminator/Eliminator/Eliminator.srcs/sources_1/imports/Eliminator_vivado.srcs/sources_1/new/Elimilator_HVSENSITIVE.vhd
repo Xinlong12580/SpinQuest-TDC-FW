f12adb97ada686e0f530720363e89edb207e50a3
Xinlong Liu <153858784+Xinlong12580@users.noreply.github.com>
2024-06-24 15:37:24 -0500

    Commented the Eliminator

--- TDC_xinlong/Eliminator/Eliminator/Eliminator.srcs/sources_1/imports/Eliminator_vivado.srcs/sources_1/new/Elimilator_HVSENSITIVE.vhd
+++ TDC_xinlong/Eliminator/Eliminator/Eliminator.srcs/sources_1/imports/Eliminator_vivado.srcs/sources_1/new/Elimilator_HVSENSITIVE.vhd
@@ -35,27 +35,27 @@ entity Eliminator is
     Port (
     clk: in std_logic:='0';
     rst: in std_logic:='0';
-    update_mode:in std_logic:='1';
-    block_window:in std_logic_vector(9 DOWNTO 0):="0000100000";
-    hit : in std_logic:='0';
+    update_mode:in std_logic:='1'; -- control signal for the update mode
+    block_window:in std_logic_vector(9 DOWNTO 0):="0000100000"; --control signal for the blocking window. Binary number format. Unit: clock cycle
+    hit : in std_logic:='0'; --the major input where the hit comes in
     
-    coarse_counter: in std_logic_vector(11 DOWNTO 0):=(others=>'0');
-    delay_chain: in std_logic_Vector (chain_length-1 DOWNTO 0):=(others=>'0');
-    registered_data: out std_logic_vector(12+chain_length-1 DOWNTO 0):=(others=>'0');
-    data_out: out std_logic:='0';
-    state_out: out std_logic_vector(1 DOWNTO 0):="00");
+    coarse_counter: in std_logic_vector(11 DOWNTO 0):=(others=>'0'); --connected to the real time coarse counter output
+    delay_chain: in std_logic_Vector (chain_length-1 DOWNTO 0):=(others=>'0');--connected to the delay chain. the first bit should be the hit
+    registered_data: out std_logic_vector(12+chain_length-1 DOWNTO 0):=(others=>'0'); --output of the registered time
+    data_out: out std_logic:='0'; -- "ready" signal sent to the next module to inform a new set of data
+    state_out: out std_logic_vector(1 DOWNTO 0):="00"); --debug module to monitor the state of the FSM
     
 end Eliminator;
 
 architecture Behavioral of Eliminator is
-Type states is (IDLE, READY, BLOCKED, FINISHING );
-Signal state :states:=IDLE;
+Type states is (IDLE, READY, BLOCKED, FINISHING ); -- FSM states defination
+Signal state :states:=IDLE;   --FSM state
 --Constant prepare_width:integer:=2;
 Signal block_width:integer:=to_integer(unsigned(block_window));
-Signal block_count: integer range 1027  DOWNTO 0:=block_width-1;
---Signal prepare_count: integer range prepare_width-1 DOWNTO 0:=prepare_width-1;
-Signal registered_data_buf: std_logic_vector(21 DOWNTO 0):=(others=>'0');
-Signal hit_delay: std_logic:='0';
+Signal block_count: integer range 1027  DOWNTO 0:=block_width-1; --counter for the blocking window
+--Signal prepare_count: integer range prepare_width-1 DOWNTO 0:=prepare_width-1; 
+Signal registered_data_buf: std_logic_vector(21 DOWNTO 0):=(others=>'0'); --output buffer
+Signal hit_delay: std_logic:='0'; --stores the hit signal status of the previous clock edge. Use for monitoring new hits arriving in the blocking woindow
 begin
 
 registered_data<=registered_data_buf;  
@@ -63,7 +63,7 @@ registered_data<=registered_data_buf;
 state_manager: process(clk) is
 begin
     if rising_edge(clk) then
-        if (rst='1') then
+        if (rst='1') then 
             state<=IDLE;
             state_out<="00";
             registered_data_buf<=(others=>'0');
@@ -74,7 +74,7 @@ begin
         else
             hit_delay<=hit;
             case state is
-            when IDLE =>
+            when IDLE => --if new hits arrives in the eliminator with state IDLE, it switches to state READY and register the coarse counter and delay chain information; elsewise it's unchanged
                 if (hit='1') then
                     state<=READY;
                     state_out<="01";
@@ -90,17 +90,17 @@ begin
                     block_count<=block_width-1;
 --                    prepare_count<=prepare_width-1;
                 end if;
-            when READY =>
+            when READY => --if the state is in READY it sends the data_out signal to the next module to inform it with the new data, and change the state to BLOCKED
  --               if (prepare_count=0) then
                     state<=BLOCKED;
                     state_out<="10";
                     registered_data_buf<=registered_data_buf;
                     data_out<='1';          
 --                    prepare_count<=prepare_width-1;
-                    if (update_mode='0') then
+                    if (update_mode='0') then -- if update mode is off, the block window counter runs freely
                         block_count<=block_count-1;
-                    elsif (update_mode='1') then
-                        if (hit='1') and (hit_delay='0') then
+                    elsif (update_mode='1') then -- if update mode is on, the block window counter is updated if a new hit is deleted
+                        if (hit='1') and (hit_delay='0') then --condition for a new hit's arrival
                             block_count<=block_width-1;
                         else
                             block_count<=block_count-1;
@@ -122,7 +122,7 @@ begin
 --                        end if;
 --                    end if;
  --               end if;
-            when BLOCKED =>
+            when BLOCKED => --The BLOCKED state ends when the block_count reaches 0. If it is in the middle of a hit, it goes into the FINISHING state; otherwise it goes back to the IDLE state
                 if (block_count=0) then
                     if (update_mode='0') then
                         if (hit='0') then
@@ -174,7 +174,7 @@ begin
                         end if;
                     end if;
                 end if;
-            when FINISHING =>
+            when FINISHING => --the FINISHING state ends at the end of a hit
                 if (hit='0') then
                     state<=IDLE;
                     state_out<="00";
